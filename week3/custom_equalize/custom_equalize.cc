266e1f4e44c28a8aca4ae4671487e5e9d32faf6e
huhuang03 <huhuang03@gmail.com>
2021-11-03 20:06:38 +0800

    up

---
+++ week3/custom_equalize/custom_equalize.cc
@@ -0,0 +1,13 @@
+//
+// Created by hwf on 2021/11/3.
+//
+
+#include "custom_equalize.h"
+#include <map>
+
+void custom_equalize(cv::InputArray in, cv::OutputArray out) {
+    std::map<int, int> count;
+    for (unsigned char gray: cv::Mat_<unsigned char>(in.getMat())) {
+        std::cout << gray << " ";
+    }
+}
