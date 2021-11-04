accc32ccc314d0cb607d341eb4d7c78d68161971
york <york@duomai.com>
2021-11-05 00:22:26 +0800

    looks I finished the custom equalize

--- week3/custom_equalize/custom_equalize.cc
+++ week3/custom_equalize/custom_equalize.cc
@@ -5,7 +5,7 @@
 #include "custom_equalize.h"
 #include <map>
 
-void custom_equalize(cv::InputArray in, cv::OutputArray out) {
+void custom_equalize(cv::InputArray in, cv::InputOutputArray out) {
     std::map<int, int> count;
     for (unsigned char gray: cv::Mat_<unsigned char>(in.getMat())) {
         count[gray] += 1;
@@ -18,18 +18,20 @@ void custom_equalize(cv::InputArray in, cv::OutputArray out) {
         count[i] = sum;
     }
 
-    // ok, now create the new output.
-    out.create(in.size(), in.type());
+    cv::Mat mat1;
+    // 16 unsigned is ok for me
+//    mat1.create(in.size(), CV_32S);
+    mat1.create(in.size(), CV_32S);
 
    // now, fill the out
    for (int r = 0; r < in.rows(); r++) {
        for (int c = 0; c < in.cols(); c++) {
-           out.getMatRef().at<unsigned char>(r, c) = count[in.getMat().at<unsigned char>(r, c)];
+           mat1.at<int>(r, c) = count[in.getMat().at<unsigned char>(r, c)];
        }
    }
 
-   cv::Mat normalized;
-   // now normalize to 256
-   cv::normalize(out, normalized, 0, 256, cv::NORM_MINMAX);
-//   normalized.assignTo(out);
+    // ok, now create the new output.
+   out.create(in.size(), in.type());
+   // ok can we normalized to cv::8U?
+   cv::normalize(mat1, out, 0, 256, cv::NORM_MINMAX, CV_8U);
 }
